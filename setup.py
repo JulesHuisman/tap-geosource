0e0b8a9c452dee6467e8567a032f669174d12982
Jules Huisman <huisman.jules@gmail.com>
2021-06-13 16:10:11 +0200

    Bumped gdal version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         "singer-python==5.12.1",
         "pathlib==1.0.1",
-        "GDAL==3.3.0"
+        "GDAL==3.2.1"
     ],
     entry_points="""
     [console_scripts]
